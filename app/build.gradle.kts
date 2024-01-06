94152e4ca9ca6ab04c6a0bb4a10cd026349daf7f
sentenzos <sentenzoss@gmail.com>
2024-01-06 17:28:16 +0300

    picocli

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.5")
 }
 
 tasks.test {
@@ -23,5 +24,5 @@ application {
 }
 
 //compileJava {
-//    options.release = 21
+//    options.release = 20
 //}
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,21 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer>  {
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
         System.out.println("Hello world!");
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        return null;
     }
 }
