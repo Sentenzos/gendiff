2e3fb25156acd95cf05573e44c80cd85fbf4344e
sentenzos <sentenzoss@gmail.com>
2024-01-26 01:20:32 +0300

    test refactoring

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,6 +1,6 @@
 package hexlet.code;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Files;
@@ -8,39 +8,38 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class DifferTest {
-    private String expectedStylish;
-    private String expectedPlain;
-    private String expectedJson;
+    private static String expectedStylish;
+    private static String expectedPlain;
+    private static String expectedJson;
 
-    @BeforeEach
-    public void getResultFile() throws Exception  {
+    @BeforeAll
+    public static void getResultFile() throws Exception  {
         Path path1 = Paths.get("./src/test/resources/stylish.txt").toAbsolutePath().normalize();
         Path path2 = Paths.get("./src/test/resources/plain.txt").toAbsolutePath().normalize();
         Path path3 = Paths.get("./src/test/resources/json.json").toAbsolutePath().normalize();
-        this.expectedStylish = Files.readString(path1);
-        this.expectedPlain = Files.readString(path2);
-        this.expectedJson = Files.readString(path3);
+        expectedStylish = Files.readString(path1);
+        expectedPlain = Files.readString(path2);
+        expectedJson = Files.readString(path3);
     }
 
     @Test
-    public void jsonStylishDiffTest() throws Exception {
+    public void jsonToStylishDiffTest() throws Exception {
         var actual = Differ.generate("./src/test/resources/j1.json",
                 "./src/test/resources/j2.json", "stylish");
         assertEquals(expectedStylish, actual);
     }
 
     @Test
-    public void yamlStylishDiffTest() throws Exception {
+    public void yamlToStylishDiffTest() throws Exception {
         var actual = Differ.generate("./src/test/resources/y1.yml", "./src/test/resources/y2.yml",
                 "stylish");
         assertEquals(expectedStylish, actual);
     }
 
     @Test
-    public void jsonPlainDiffTest() throws Exception {
+    public void jsonToPlainDiffTest() throws Exception {
         var actual = Differ.generate("./src/test/resources/j1.json",
                 "./src/test/resources/j2.json", "plain");
         assertEquals(expectedPlain, actual);
@@ -51,12 +50,6 @@ public class DifferTest {
         var actual = Differ.generate("./src/test/resources/j1.json",
                 "./src/test/resources/j2.json", "json");
         assertEquals(expectedJson, actual);
-    }
-
-    @Test
-    public void throwTest() {
-        assertThrows(Exception.class, () -> Differ.generate("",  "./src/test/resources/j2.json",
-                "stylish"));
     }
 }
 
